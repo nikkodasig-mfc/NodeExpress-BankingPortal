f2833b44753ca81f38383ede30124f2673c4d5d5
Nikko Dasig <john_dominic_dasig@manulife.ca>
2021-06-17 14:45:49 +0800

    Module 3

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ app.set('view engine', 'ejs');
 // Configure the static directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true }));
+
 const accountData = fs.readFileSync(path.join(__dirname, '/json/accounts.json'), 'UTF8');
 const accounts = JSON.parse(accountData);
 
@@ -34,6 +36,38 @@ app.get('/checking', (req, res) => {
 
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] });
+});
+
+app.get('/transfer', (req, res) => {
+  res.render('transfer', { user: users[0] });
+});
+
+app.get('/payment', (req, res) => {
+  res.render('payment', { account: accounts.credit });
+});
+
+app.post('/payment', (req, res) => {
+  const amount = parseInt(req.body.amount, 10);
+  accounts.credit.balance -= amount;
+  accounts.credit.available += amount;
+
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'UTF8');
+
+  res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+});
+
+app.post('/transfer', (req, res) => {
+  const amount = parseInt(req.body.amount, 10);
+
+  accounts[req.body.from].balance -= amount;
+  accounts[req.body.to].balance += amount;
+
+  const accountsJSON = JSON.stringify(accounts);
+
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'UTF8');
+
+  res.render('transfer', { message: 'Transfer Completed' });
 });
 
 // Create the server that will listen on port 3000
